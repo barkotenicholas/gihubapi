1d09b81683053f4b66cf40937a3966cc5e336e4b
barkotenicholas <barkotenicholas@gmail.com>
2022-02-12 07:51:48 +0300

    added repos component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponentComponent
+  },
+  {
+    path:'search/:git-search',
+    component:HomeComponentComponent
+  }
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    HomeComponentComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
