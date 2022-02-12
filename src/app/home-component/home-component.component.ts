3b5e28ea41c6045bac1ce5bbc9c87ac921ba6ae8
barkotenicholas <barkotenicholas@gmail.com>
2022-02-12 09:27:19 +0300

    addded httpmodule

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { HttpService } from '../http.service';
 import { User } from '../user';
 
@@ -11,9 +12,26 @@ export class HomeComponentComponent implements OnInit {
 
   private githubUserQuery!:string;
   private user!:User;
-  constructor(private httpService : HttpService) { }
+  constructor(private httpService : HttpService ,private activateddRoute:ActivatedRoute) { 
+
+  }
 
   ngOnInit(): void {
+    this.activateddRoute.params.subscribe((params:Params)=>{
+      if(params['git-search']){
+        this.httpService.getProfileInfo(params["git-search"]).subscribe((profile:User)=>{
+          this.user = profile;
+          console.log(this.user);
+        });
+      
+      }
+      else{
+        this.httpService.getProfileInfo("barkotenicholas").subscribe((profile:User)=>{
+          this.user = profile;
+          console.log(this.user);
+        });     
+       }
+    });
   }
 
 
